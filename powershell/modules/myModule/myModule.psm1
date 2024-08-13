ddf13865713222be48248673e865750cc227550f
Eric Thorneloe <129253602+ethorneloe@users.noreply.github.com>
2024-08-13 14:46:40 +0800

    Update myModule.psm1

--- powershell/modules/myModule/myModule.psm1
+++ powershell/modules/myModule/myModule.psm1
@@ -1,1 +1,14 @@
+# Load private functions
+$privateFunctions = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Functions/Private') -Filter *.ps1 -Recurse
+foreach ($privateFunction in $privateFunctions) {
+    . $privateFunction.FullName
+}
 
+# Load public functions
+$publicFunctions = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Functions/Public') -Filter *.ps1 -Recurse
+foreach ($publicFunction in $publicFunctions) {
+    . $publicFunction.FullName
+}
+
+# Export public functions
+Export-ModuleMember -Function $publicFunctions.Basename
